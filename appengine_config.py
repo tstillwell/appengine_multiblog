1f5930b4ac9b91562768c01c8ff32ad6ceaa721e
tstillwell2k17 <tristanstillwell@gmail.com>
2017-09-29 22:28:24 -0400

    docs: Add docstring to app_engine_config.py

--- appengine_config.py
+++ appengine_config.py
@@ -1,3 +1,10 @@
+"""    This module is used to add any third party libraries to
+       the project codebase. See the libs folder and the README file.
+
+       This file is parsed when deploying the application or running the
+       local development server.
+"""
+
 # appengine_config.py
 from google.appengine.ext import vendor
 
